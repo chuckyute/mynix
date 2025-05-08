a4b63fc8232584bb1c7cece7ce3a65c4a889a796
Charles Young <charlieyoung0807@gmail.com>
2025-05-08 15:11:47 -0500

    initial neovim configuration

--- home.nix
+++ home.nix
@@ -53,11 +53,66 @@
     userEmail = "charlieyoung0807@gmail.com";
   };
 
-  programs.neovim = {
+  programs.neovim =
+    let
+      toLua = str: "lua << EOF\n${str}\nEOF\n";
+      toLuaFile = file: "lua << EOF\n${builtins.readFile file}\nEOF\n";
+    in
+    {
     enable = true;
     viAlias = true;
     vimAlias = true;
     vimdiffAlias = true;
+    extraPackages = with pkgs; [
+      lua-language-server
+      nixd
+
+      xclip
+      wl-clipboard
+    ];
+
+    plugins = with pkgs.vimPlugins; [
+      neodev-nvim
+      {
+        plugin = nvim-lspconfig;
+        config = toLuaFile ./nvim/plugin/lsp.lua;
+      }
+      {
+        plugin = comment-nvim;
+        config = toLua "require(\"Comment\").setup()";
+      }
+      {
+        plugin = gruvbox-nvim;
+        config = "colorscheme gruvbox";
+      }
+      cmp-nvim-lsp
+      {
+        plugin = nvim-cmp;
+        config = toLuaFile ./nvim/plugin/cmp.lua;
+      }
+      {
+        plugin = telescope-nvim;
+        config = toLuaFile ./nvim/plugin/telescope.lua;
+      }
+      telescope-fzf-native-nvim
+      friendly-snippets
+      {
+        plugin = (nvim-treesitter.withPlugins (p: [
+          p.tree-sitter-nix
+          p.tree-sitter-vim
+          p.tree-sitter-bash
+          p.tree-sitter-lua
+          p.tree-sitter-python
+          p.tree-sitter-json
+        ]));
+        config = toLuaFile ./nvim/plugin/treesitter.lua;
+      }
+      vim-nix
+    ];
+
+    extraLuaConfig = ''
+      ${builtins.readFile ./nvim/options.lua}
+    '';
   };
 
   # Home Manager can also manage your environment variables through
