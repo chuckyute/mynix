6c64f5d06479ec83725c5c66f4018f5c7f312b39
chuckyute <charlieyoung0807@gmail.com>
2025-05-23 17:54:31 -0500

    trying to add steam?

--- home.nix
+++ home.nix
@@ -38,6 +38,17 @@
     };
   };
 
+  programs.steam = {
+    enable = true;
+    gamescopeSession.enable = true;
+    remotePlay.openFirewall = true;
+    dedicatedServer.openFirewall = true;
+    localNetworkGameTransfers.openFirewall = true;
+  };
+
+  programs.gamemode.enable = true;
+  programs.gamescope.enable = true;
+
   stylix = {
     targets = {
       neovim.enable = false;
