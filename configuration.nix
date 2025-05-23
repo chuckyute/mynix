60d2ac1062bb78a9e6874b7634beb4131bfd5579
chuckyute <charlieyoung0807@gmail.com>
2025-05-23 18:03:51 -0500

    adding steam to configuration

--- configuration.nix
+++ configuration.nix
@@ -202,6 +202,16 @@
     };
   };
 
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+    gamescopeSession.enable = true;
+  };
+
+  programs.gamescope.enable = true;
+
   environment.variables = {
     LIBVA_DRIVER_NAME = "nvidia";
     MOZ_ENABLE_WAYLAND = "1";
