7c539807d408565d4015d3913c5073bd289dfcbf
chuckyute <charlieyoung0807@gmail.com>
2025-05-23 16:34:56 -0500

    adding icons to waybar workspaces?

--- modules/waybar.nix
+++ modules/waybar.nix
@@ -22,6 +22,39 @@
           "clock"
           "tray"
         ];
+        # Workspaces showing number + app icons
+        "sway/workspaces" = {
+          "disable-scroll" = true;
+          "all-outputs" = true;
+          # Format: workspace number followed by application icons
+          "format" = "{name} {icon}";
+          "format-icons" = {
+            # Default icons for different workspace states
+            "urgent" = "";
+            "focused" = "";
+            "default" = "";
+          };
+          # Map application classes to icons
+          # Use the most common/likely class names
+          "window-rewrite" = {
+            "class<firefox>" = "󰈹"; # Firefox (X11)
+            "app_id<firefox>" = "󰈹"; # Firefox (Wayland)
+            "class<discord>" = "󰙯"; # Discord
+            "class<code>" = "󰨞"; # VS Code
+            "app_id<ghostty>" = ""; # Ghostty (Wayland native)
+            "class<Thunar>" = "󰝰"; # Thunar file manager
+            "class<steam>" = "󰓓"; # Steam
+            "class<Godot>" = "󰯂"; # Godot
+            "class<pavucontrol>" = "󰕾"; # PulseAudio Volume Control
+            "class<nm-connection-editor>" = "󰀂"; # NetworkManager
+            # Terminal applications by title
+            "title<.*nvim.*>" = ""; # Neovim in any terminal
+            "title<.*vim.*>" = ""; # Vim in any terminal
+          };
+          # Show application names on hover
+          "tooltip" = true;
+          "tooltip-format" = "Workspace {name}\n{windows} window(s)";
+        };
 
         "pulseaudio" = {
           format = "{volume}% {icon}";
