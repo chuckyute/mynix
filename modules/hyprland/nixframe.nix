e3992280651adfea8f7f2533744ebb84fcd07ad9
chuckyute <charlieyoung0807@gmail.com>
2026-03-10 20:41:52 -0500

    adjusting fonts to fit with scaling

--- modules/hyprland/nixframe.nix
+++ modules/hyprland/nixframe.nix
@@ -1,14 +1,14 @@
 { ... }:
 {
   stylix.fonts.sizes = {
-    applications = 22;
-    desktop = 22;
-    popups = 22;
+    applications = 16;
+    desktop = 16;
+    popups = 16;
   };
 
   wayland.windowManager.hyprland.settings = {
     monitor = [
-      "eDP-1, 2560x1600@165, 0x0, 2.0"
+      "eDP-2, 2560x1600@165, 0x0, 1.6"
       ", preferred, auto, 1"
     ];
 
